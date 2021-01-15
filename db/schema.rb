239ea377598ad9cb64c5468c51245574a24e2c7d
mika.yoshizawa <kisuke1290@gmail.com>
2021-01-15 16:31:49 +0000

    new tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_170611) do
+ActiveRecord::Schema.define(version: 2021_01_15_114841) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -29,5 +29,4 @@ ActiveRecord::Schema.define(version: 2021_01_12_170611) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "tasks", "users"
 end
