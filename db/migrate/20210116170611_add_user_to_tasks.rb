272392404f6bc5696c938a604dfeacf62705b262
mika.yoshizawa <kisuke1290@gmail.com>
2021-01-16 07:30:08 +0000

    heroku tasklist

--- db/migrate/20210116170611_add_user_to_tasks.rb
+++ db/migrate/20210116170611_add_user_to_tasks.rb
@@ -1,5 +1,4 @@
 class AddUserToTasks < ActiveRecord::Migration[5.2]
-
   def change
     add_reference :tasks, :user, foreign_key: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_114841) do
+ActiveRecord::Schema.define(version: 2021_01_16_170611) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
